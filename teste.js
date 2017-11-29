9fbac95bc53f7488c213ee67786984b8ef402f69
Daniel Venturini <daniellagarto0@gmail.com>
2017-11-29 16:25:09 -0200

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,11 +1,11 @@
-window.addEventListener('deviceorientation', function (evt){
-                                                if (evt.alpha < 5 || evt.alpha > 355) {
-                                                  window.alert("North!");
-                                                } else if (evt.alpha < 180) {
-                                                  window.alert("Turn Left");
-                                                } else {
-                                                  window.alert("Turn Right");
-                                                }
-                                            },
-false
-);
+var setGameAlphaOffset = function (evt){
+	if (evt.alpha < 5 || evt.alpha > 355) {
+    	window.alert("North!");
+    } else if (evt.alpha < 180) {
+    	window.alert("Turn Left");
+    } else {
+    	window.alert("Turn Right");
+    }
+}
+
+window.addEventListener('deviceorientation', setGameAlphaOffset, true);
