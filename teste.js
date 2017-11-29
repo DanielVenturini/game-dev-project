55c306ce228231825c33c7639e87776c8653927d
Daniel Venturini <daniellagarto0@gmail.com>
2017-11-29 16:11:16 -0200

    agora vai

--- teste.js
+++ teste.js
@@ -1,12 +1,10 @@
-window.addEventListener("deviceorientation", brasilvaronil);
-
-function brasilvaronil(evt){
-  if (evt.alpha < 5 || evt.alpha > 355) {
-    windows.alert("North!");
-  } else if (evt.alpha < 180) {
-    windows.alert("Turn Left");
-  } else {
-    windows.alert("Turn Right");
-  }
-
-}
+window.addEventListener("deviceorientation", function (evt){
+                                                if (evt.alpha < 5 || evt.alpha > 355) {
+                                                  windows.alert("North!");
+                                                } else if (evt.alpha < 180) {
+                                                  windows.alert("Turn Left");
+                                                } else {
+                                                  windows.alert("Turn Right");
+                                                }
+                                            }
+);
