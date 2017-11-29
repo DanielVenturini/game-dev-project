df31b8c85d5cc225594b6d6962342580508b1be1
danielventurini <daniellagarto0@gmail.com>
2017-11-29 16:50:00 -0200

    brasil

--- js/ball.js
+++ js/ball.js
@@ -21,9 +21,20 @@ class Ball extends Phaser.Sprite {
                 right: game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
             }
         }
-    
+
         create(){
-            window.addEventListener('deviceorientation', moveCel, true)
+
+            var setGameAlphaOffset = function (evt){
+                if (evt.alpha < 5 || evt.alpha > 355) {
+                    window.alert("North!");
+                } else if (evt.alpha < 180) {
+                    window.alert("Turn Left");
+                } else {
+                    window.alert("Turn Right");
+                }
+            }
+
+            window.addEventListener('deviceorientation', setGameAlphaOffset, true)
         }
     
         update() {
